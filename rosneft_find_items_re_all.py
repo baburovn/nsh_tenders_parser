374188c801ec7f3e0149594db3ef19a24d0b032d
baburovn <65155978+baburovn@users.noreply.github.com>
2021-05-31 18:39:57 +0500

    Update rosneft_find_items_re_all.py

--- rosneft_find_items_re_all.py
+++ rosneft_find_items_re_all.py
@@ -1,16 +1,17 @@
 import re
 import urllib.request
+from urllib.parse import quote
 
+#Вводим ссылки для парсинга. В строку после quote добавляем слово для поиска, пробелы заменяем +
+url_link = urllib.request.urlopen('https://www.tektorg.ru/procedures?q=' +quote('сейсморазведка') + "&lang=ru&sort=date&order=desc'")
+#Данная переменная требуется для поиска по строкам:
 rosnefttkp = "https://www.tektorg.ru/rosnefttkp/procedures/"
-url_link = urllib.request.urlopen('https://www.tektorg.ru/procedures?q=petroleum+experts&lang=ru&sort=date&order=desc')
+
 for line in url_link:
     url_link = line.decode().strip()
     if rosnefttkp in url_link:
-        #index print(data_title_endpos,data_title_endpos)
+        #регулярное выражение для Названия тендера
         print(re.findall('data-title="(.*?)"', url_link))
-        #print(url_link)
-
-        #start_link = url_link.find("https:")
-        #end_link = url_link.find('?la', start_link)
-        print(re.findall('data-title="(.*?)"', url_link))
-        print(url_link[start_link:end_link])
+        # регулярное выражение для ссылку на тендер и документацию
+        print(re.findall('data-url="(.*?)"', url_link))
+#далее сделаем запись в файл
